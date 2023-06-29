2f828f83c3654f48ad008d9f60d73177098d9619
Luis Mejia <luisrvp5@gmail.com>
2023-06-29 16:58:07 -0600

    primera parte completa

---
+++ index.js
@@ -0,0 +1,7 @@
+function mision(){
+    alert("Our mision is to inform or educate our audience in the procedures of hot to petition Guardianship and/or Conservatorship ")
+}
+
+function history(){
+    alert("I need info Liz.")
+}
