bbcad699f51351b173bcb950a42522fd9c523b8d
Luis Mejia <luisrvp5@gmail.com>
2023-07-07 17:08:06 -0600

    otro avance

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const enlacesMenu = document.querySelectorAll('nav ul li a');
+  const secciones = document.querySelectorAll('section');
+
+  function showHomeSection() {
+    secciones.forEach(function(seccion) {
+      seccion.style.display = 'none';
+    });
+
+    const homeSection = document.getElementById('home');
+    homeSection.style.display = 'block';
+  }
+
+  enlacesMenu.forEach(function(enlace) {
+    enlace.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      const idSeccion = enlace.getAttribute('href').substring(1);
+      const seccionSeleccionada = document.getElementById(idSeccion);
+
+      secciones.forEach(function(seccion) {
+        seccion.style.display = 'none';
+      });
+
+      seccionSeleccionada.style.display = 'block';
+    });
+  });
+
+  showHomeSection(); // Call the function to show the "home" section on page load
+});
+
+  
+
 function mision(){
-    alert("Our mision is to inform or educate our audience in the procedures of hot to petition Guardianship and/or Conservatorship ")
+  alert("Our mision is to educate our audience with rich content about Guardianship and Conservatorship")
 }
 
-function history(){
-    alert("I need info Liz.")
+function vision(){
+  alert("I need info, Liz")
 }
