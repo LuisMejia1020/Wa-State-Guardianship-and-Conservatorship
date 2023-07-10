8b896e70d9ea3a3599ffb1aa0249f0f57b59e6ec
Luis Mejia <luisrvp5@gmail.com>
2023-07-10 17:25:53 -0600

    avance forms guardianship

--- index.js
+++ index.js
@@ -29,8 +29,6 @@ document.addEventListener('DOMContentLoaded', function() {
   showHomeSection(); // Call the function to show the "home" section on page load
 });
 
-  
-
 function mision(){
   alert("Our mision is to educate our audience with rich content about Guardianship and Conservatorship")
 }
@@ -50,4 +48,48 @@ function openPDF2() {
 
 function openPDF3() {
   window.open('forms/90 Day- Checklist For File (1).pdf', '_blank');
+}
+
+function openPDF4() {
+  window.open('forms/Form - Acceptance of Appointment of Guardian - Conservator.pdf', '_blank');
+}
+
+function openPDF5() {
+  window.open('forms/Form - G-C Report.pdf', '_blank');
+}
+
+function openPDF6() {
+  window.open('forms/Form - Guardian - Conservator plan and motion to approve.pdf', '_blank');
+}
+
+function openPDF7() {
+  window.open('forms/Form - Motion to appoint lawyer.pdf', '_blank');
+}
+
+function openPDF8() {
+  window.open('forms/Form - Motion to Appoint Successor Guardian - Conservator.pdf', '_blank');
+}
+
+function openPDF9() {
+  window.open('forms/Form - Motion Motion to Approve G-C report.pdf', '_blank');
+}
+
+function openPDF9() {
+  window.open('forms/Form - Motion to Approve G-C report.pdf', '_blank');
+}
+
+function openPDF10() {
+  window.open('forms/Form - Notice of hearing and declaration of mailing.pdf', '_blank');
+}
+
+function openPDF11() {
+  window.open('forms/Form - Notice of Petition for G-C or PA for adult.pdf', '_blank');
+}
+
+function openPDF12() {
+  window.open('forms/Form - Notice of rights.pdf', '_blank');
+}
+
+function openPDF13() {
+  window.open('forms/Form - substantial change in circumstances.pdf', '_blank');
 }
